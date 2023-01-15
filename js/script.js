07981a9f3be06c0879995d8b17820341392f0d04
parker <parkerschellenberg@gmail.com>
2023-01-15 14:36:30 -0600

    Use the ternary operator for better readability

--- js/script.js
+++ js/script.js
@@ -7,24 +7,24 @@ Generate and return a new computer choice - rock, paper, or scissors
 */
 function getComputerChoice() {
   let randNum = Math.floor(Math.random()*3) + 1; // Random number - either 1 2 or 3
-  if (randNum === 1) return "rock"
-  else if (randNum === 2) return "paper"
-  else if (randNum === 3) return "scissors"
-  else console.log("An error occurred in getComputerChoice()")
+  return (randNum === 1) ? "rock"
+  : (randNum === 2) ? "paper"
+  : (randNum === 3) ? "scissors"
+  : "An error occurred";
 }
 
 /*
 Evaluate a single round of rock paper scissors and return win/lose result
 */
 function playSingleRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() === computerSelection) return "It's a tie! Try again"
-  else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") return "You win! Rock beats Scissors";
-  else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") return "You win! Paper beats Rock";
-  else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") return "You win! Scissors beats Paper";
-  else if (computerSelection === "rock" && playerSelection.toLowerCase() === "scissors") return "You lose! Rock beats Scissors";
-  else if (computerSelection === "paper" && playerSelection.toLowerCase() === "rock") return "You lose! Paper beats Rock";
-  else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper") return "You lose! Scissors beats Paper";
-  else return "Please enter either rock, paper, or scissors"
+  return (playerSelection.toLowerCase() === computerSelection) ? "It's a tie! Try again"
+  : (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") ? "You win! Rock beats Scissors"
+  : (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") ? "You win! Paper beats Rock"
+  : (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") ? "You win! Scissors beats Paper"
+  : (computerSelection === "rock" && playerSelection.toLowerCase() === "scissors") ? "You lose! Rock beats Scissors"
+  : (computerSelection === "paper" && playerSelection.toLowerCase() === "rock") ? "You lose! Paper beats Rock"
+  : (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper") ? "You lose! Scissors beats Paper"
+  : "Please enter either rock, paper, or scissors";
 }
 
 /*
